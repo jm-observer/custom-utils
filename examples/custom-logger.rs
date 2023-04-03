59bc202ef0db3122f91ac4d6965f61d3f871c680
huangjm <362254883@qq.com>
2023-04-03 08:20:17 +0800

    1. added path param

--- examples/custom-logger.rs
+++ examples/custom-logger.rs
@@ -1,4 +1,5 @@
 use flexi_logger::writers::LogWriter;
+use flexi_logger::Duplicate::Debug;
 use flexi_logger::{DeferredNow, FormatFunction, Logger};
 use log::{debug, info, Record};
 
@@ -6,16 +7,14 @@ pub struct CustomWriter;
 
 impl LogWriter for CustomWriter {
     fn write(&self, now: &mut DeferredNow, record: &Record) -> std::io::Result<()> {
-        println!("[{}]", record.args());
+        println!("[{}]", record.args().to_string());
         Ok(())
     }
-
     fn flush(&self) -> std::io::Result<()> {
         Ok(())
     }
-
     fn max_log_level(&self) -> log::LevelFilter {
-        log::LevelFilter::Debug
+        log::LevelFilter::Info
     }
 }
 
@@ -23,8 +22,9 @@ fn main() {
     let mut logger = Logger::try_with_str("info").unwrap();
     logger
         .log_to_writer(Box::new(CustomWriter))
+        .duplicate_to_stdout(Debug)
         .start()
         .unwrap();
-    info!("info");
-    debug!("debug");
+    info!("infsssssssssso");
+    debug!("debssssssssssug");
 }
